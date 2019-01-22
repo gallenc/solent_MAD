954b42e2ed6fabfc15e8c8c533e12496f5683470
Patrick West <3westp36@solent.ac.uk>
2019-01-22 15:54:26 +0000

    lol

--- session2/app/src/main/java/uk/ac/solent/converter/MainActivity.java
+++ session2/app/src/main/java/uk/ac/solent/converter/MainActivity.java
@@ -2,12 +2,27 @@ package uk.ac.solent.converter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b = (Button)findViewById(R.id.btn1);
+        b.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        TextView tv = (TextView)findViewById(R.id.tv1);
+        EditText et = (EditText)findViewById(R.id.et1);
+        double feet = Double.parseDouble(et.getText().toString());
+        double meters = feet*0.305;
+        tv.setText("In meters that is: " + meters);
     }
 }
