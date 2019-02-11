1febd86c7a25cb652f67e97ac310690f26e8f48e
[patrick west] <paddy12309@icloud.com>
2019-02-11 13:25:15 +0000

    session4 :wq

--- session3/app/src/main/java/com/example/mapping/MainActivity.java
+++ session3/app/src/main/java/com/example/mapping/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import org.osmdroid.config.Configuration;
 import org.osmdroid.util.GeoPoint;
@@ -16,8 +18,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
@@ -25,11 +26,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
 
         setContentView(R.layout.activity_main);
+        Button b = (Button) findViewById(R.id.btn1);
+        b.setOnClickListener(this);
+
 
         mv = findViewById(R.id.map1);
 
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(16);
-        mv.getController().setCenter(new GeoPoint(51.05,-0.72));
+        mv.getController().setCenter(new GeoPoint(51.05, -0.72));
     }
+
+
+        public void onClick(View view)
+        {
+            EditText et1 = (EditText)findViewById(R.id.et1);
+            EditText et2 = (EditText)findViewById(R.id.et2);
+            double lon = Double.parseDouble(et1.getText().toString());
+            double lat = Double.parseDouble(et2.getText().toString());
+            mv.getController().setCenter(new GeoPoint(lat, lon));
+        }
+
+
+
 }
