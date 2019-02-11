438073ed495861afb90da44306826599a7e1c8e5
[patrick west] <paddy12309@icloud.com>
2019-02-11 13:46:27 +0000

    try again

---
+++ session4/app/src/main/java/com/example/mapping/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.mapping;
+
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+
+    MapView mv;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        // This line sets the user agent, a requirement to download OSM maps
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+
+        setContentView(R.layout.activity_main);
+        Button b = (Button) findViewById(R.id.btn1);
+        b.setOnClickListener(this);
+
+
+        mv = findViewById(R.id.map1);
+
+        mv.setBuiltInZoomControls(true);
+        mv.getController().setZoom(16);
+        mv.getController().setCenter(new GeoPoint(51.05, -0.72));
+    }
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+
+    }
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.choosemap)
+        {
+            // react to the menu item being selected...
+            return true;
+            Intent intent = new Intent(this,mapChooseActivity.class);
+            startActivity(intent);
+        }
+        return false;
+    }
+
+
+
+    public void onClick(View view)
+        {
+            EditText et1 = (EditText)findViewById(R.id.et1);
+            EditText et2 = (EditText)findViewById(R.id.et2);
+            double lon = Double.parseDouble(et1.getText().toString());
+            double lat = Double.parseDouble(et2.getText().toString());
+            mv.getController().setCenter(new GeoPoint(lat, lon));
+        }
+
+
+
+}
--- session4/app/src/main/java/mapChooseActivity.java
+++ session4/app/src/main/java/mapChooseActivity.java
@@ -1,2 +1,17 @@
-public class mapChooseActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+
+import com.example.mapping.R;
+
+public class MapChooseActivity extends AppCompatActivity
+{
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_choose);
+    }
 }
