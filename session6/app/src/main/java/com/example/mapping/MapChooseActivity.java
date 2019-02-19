2aa23eaad3cae2adbf90c4a7f5a576af977caded
[patrick west] <paddy12309@icloud.com>
2019-02-19 16:20:29 +0000

    session 6

---
+++ session6/app/src/main/java/com/example/mapping/MapChooseActivity.java
@@ -0,0 +1,39 @@
+package com.example.mapping;
+
+import android.app.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+
+public class MapChooseActivity extends AppCompatActivity implements View.OnClickListener
+{
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_choose);
+        Button regular = (Button)findViewById(R.id.btnRegular);
+        regular.setOnClickListener(this);
+        Button hikebikemap = (Button)findViewById(R.id.btnHikeBikeMap);
+        hikebikemap.setOnClickListener(this);
+    }
+    public void onClick(View v)
+    {
+        Intent intent = new Intent();
+        Bundle bundle=new Bundle();
+        boolean hikebikemap=false;
+        if (v.getId()==R.id.btnHikeBikeMap)
+        {
+            hikebikemap=true;
+        }
+        bundle.putBoolean("com.example.hikebikemap",hikebikemap);
+        intent.putExtras(bundle);
+        setResult(RESULT_OK,intent);
+        finish();
+    }
+
+
+}
